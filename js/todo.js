add0ec7c9cddd2bd336ae93bcc122ca2e3ea7691
AUDWO <kmjstj3@naver.com>
2022-11-19 19:06:55 +0900

    2022-11-19

--- js/todo.js
+++ js/todo.js
@@ -4,10 +4,29 @@ const toDoInput = toDoForm.querySelector("input")
 
 //form은 submit이벤트를 거치고 submit이벤트는 페이지를 새로고침하는 것
 
+const toDos = [];
+
+function saveToDo() {
+  //JSON.stringify는 어떤 javascript문법이든지 간에 문자열로 바꿔준다
+  localStorage.setItem("todos", JSON.stringify(toDos))
+}
+
+function deleteTodo(event) {
+   //event.target은 이벤트가 발생한 html태그를 말한다
+   const li = event.target.parentElement;
+   li.remove();
+
+}
+
+
 function paintToDo(newTodo) {
   const li = document.createElement("li")
   const span = document.createElement("span");
+  const button = document.createElement("button")
+  button.innerText = "X";
+  button.addEventListener("click", deleteTodo)
   li.appendChild(span);
+  li.appendChild(button)
   span.innerText = newTodo;
   toDoList.appendChild(li);
 }
@@ -18,7 +37,10 @@ function paintToDo(newTodo) {
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
+  toDos.push(newTodo);
   toDoInput.value = "";
+  
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
